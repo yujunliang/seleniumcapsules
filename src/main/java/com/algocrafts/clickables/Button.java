1c11834a408978a02d994c088e528fa11813f44c
Yujun Liang <yujunliang@yujun.home>
2014-05-24 20:27:30 -0500

    [Yujun] Removed unused interface from Button class.

--- src/main/java/com/algocrafts/clickables/Button.java
+++ src/main/java/com/algocrafts/clickables/Button.java
@@ -1,12 +1,11 @@
 package com.algocrafts.clickables;
 
 
-import com.algocrafts.algorithm.Retryable;
 import com.algocrafts.pages.Element;
-import com.algocrafts.pages.Searchable;
 import com.algocrafts.pages.Locator;
+import com.algocrafts.pages.Searchable;
 
-public class Button<Where extends Searchable<Where>> extends AbstractClicker<Where> implements Retryable<Element> {
+public class Button<Where extends Searchable<Where>> extends AbstractClicker<Where>  {
 
     public Button(Where where, Locator<Where, Element> locator) {
         super(where, locator);
